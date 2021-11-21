89a8626eb3f01d52a5358cba30fc875ddef058fd
AdnanAzem <88532380+AdnanAzem@users.noreply.github.com>
2021-11-21 21:29:33 +0200

    add my_mat.h

---
+++ my_mat.h
@@ -0,0 +1,10 @@
+#ifndef _my_mat
+#define _my_mat
+#include <stdio.h>
+void isThereConnection();
+void buildGraph();
+void findPath(int x, int y);
+void Dijkstra(int x);
+void shortPath();
+
+#endif
