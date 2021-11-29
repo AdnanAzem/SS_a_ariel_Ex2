84a503ca9cc56cec029b3bd2fc1617a7262e2c7c
AdnanAzem <88532380+AdnanAzem@users.noreply.github.com>
2021-11-29 12:00:40 +0200

    Update my_mat.h

--- my_mat.h
+++ my_mat.h
@@ -1,12 +1,10 @@
-
 #ifndef my_mat
 #define my_mat
+#define N 10
 #include <stdio.h>
+int min(int a, int b);
+void floydWarshalle(int mat[][N]);
 void isThereConnection();
 void buildGraph();
-void findPath(int x, int y);
-void min_temp();
-void Dijkstra(int x);
 void shortPath();
-
 #endif
